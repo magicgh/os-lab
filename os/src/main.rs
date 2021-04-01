7eec59137e414f48e77eb113b28dd40d6a843559
magicgh <magicgher@hotmail.com>
2021-04-01 14:41:28 +0800

    :sparkles: complete lab-3.1

--- os/src/main.rs
+++ os/src/main.rs
@@ -11,26 +11,50 @@
 // 打印 panic 信息
 #![feature(panic_info_message)]
 
+//! - `#![feature(alloc_error_handler)]`
+//!   我们使用了一个全局动态内存分配器，以实现原本标准库中的堆内存分配。
+//!   而语言要求我们同时实现一个错误回调，这里我们直接 panic
+#![feature(alloc_error_handler)]
+
 #[macro_use]
 mod console;
 mod panic;
 mod sbi;
 mod interrupt;
+mod memory;
+
+extern crate alloc;
 
 // 汇编入口
 global_asm!(include_str!("entry.asm"));
 
 // Rust 入口函数
 
+/// Rust 的入口函数
+///
+/// 在 `_start` 为我们进行了一系列准备之后，这是第一个被调用的 Rust 函数
 #[no_mangle]
 pub extern "C" fn rust_main() -> ! {
     // 初始化各种模块
     interrupt::init();
+    memory::init();
 
-    unsafe {
-        llvm_asm!("ebreak"::::"volatile");
-    };
+    // 动态内存分配测试
+    use alloc::boxed::Box;
+    use alloc::vec::Vec;
+    let v = Box::new(5);
+    assert_eq!(*v, 5);
+    core::mem::drop(v);
 
-    loop{}
-    // panic!("end of rust_main");
+    let mut vec = Vec::new();
+    for i in 0..10000 {
+        vec.push(i);
+    }
+    assert_eq!(vec.len(), 10000);
+    for (i, value) in vec.into_iter().enumerate() {
+        assert_eq!(value, i);
+    }
+    println!("heap test passed, Zhang Xuan");
+    println!("shutdown!");
+    sbi::shutdown();
 }
